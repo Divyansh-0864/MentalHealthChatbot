176e450dd3cb5bfc5b741f14fc895453c7b1bf32
Divyansh Sharma <divyanshsharma2203@gmail.com>
2025-03-23 16:06:01 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import chatRoutes from "./routes/chatbotRoutes.js";
 
 dotenv.config();
 
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
