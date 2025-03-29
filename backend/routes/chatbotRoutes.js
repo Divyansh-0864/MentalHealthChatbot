7f11cfdae831b29b38af9db5a0d810b024778915
Divyansh <114722731+Divyansh-0864@users.noreply.github.com>
2025-03-29 12:19:01 +0530

    Delete backend directory

--- backend/routes/chatbotRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { chatWithGroq } from "../controllers/chatbotController.js";
-
-const router = express.Router();
-
-router.post("/chat", chatWithGroq);
-
-export default router;
